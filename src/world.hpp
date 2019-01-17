238b5cbedb31116a662686ae3f704e0c89e99a53
Cowan <ctcumming@gmail.com>
2019-01-16 16:47:19 -0800

    Got simple panda texture working, but still need water rendering for some reason.

---
+++ src/world.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "common.hpp"
+#include "panda.hpp"
+#include "water.hpp"
+
+#include <vector>
+#include <random>
+
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+#include <SDL_mixer.h>
+
+// Container for all our entities and game logic. Individual rendering / update is 
+// deferred to the relative update() methods
+class World
+{
+public:
+	World();
+	~World();
+
+	// Creates a window, sets up events and begins the game
+	bool init(vec2 screen);
+
+	// Releases all associated resources
+	void destroy();
+
+	// Steps the game ahead by ms milliseconds
+	bool update(float ms);
+
+	// Renders our scene
+	void draw();
+
+	// Should the game be over ?
+	bool is_over()const;
+
+
+
+private:
+	// !!! INPUT CALLBACK FUNCTIONS
+	void on_key(GLFWwindow*, int key, int, int action, int mod);
+	void on_mouse_move(GLFWwindow* window, double xpos, double ypos);
+
+	// Window handle
+	GLFWwindow* m_window;
+
+	// Screen texture
+	// The draw loop first renders to this texture, then it is used for the water shader
+	GLuint m_frame_buffer;
+	Texture m_screen_tex;
+
+	// C++ rng
+	std::default_random_engine m_rng;
+	std::uniform_real_distribution<float> m_dist; // default 0..1
+
+	Panda m_panda;
+
+	Water m_water;
+};
