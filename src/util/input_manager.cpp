2c2bcee74be21611c2adee62a48d07b9b61fad9c
Alexander <o1j0b@ugrad.cs.ubc.ca>
2019-01-30 12:00:26 -0800

    Implemented input manager

--- src/util/input_manager.cpp
+++ src/util/input_manager.cpp
@@ -8,20 +8,70 @@
 
 
 InputManager::InputManager() :
+    key_states_(),
     should_exit_(false)
 {}
 
 void InputManager::update() {
-    //TODO: implement more
     SDL_Event event;
 
+    // update states that were "JUST_XYZ" last frame
+    for (auto& entry : key_states_) {
+        switch (entry.second) {
+            case JUST_PRESSED:
+                entry.second = PRESSED;
+            case JUST_RELEASED:
+                entry.second = RELEASED;
+        }
+    }
+
     while (SDL_PollEvent(&event) != 0) {
-        if (event.type == SDL_QUIT) {
-            should_exit_ = true;
+        switch(event.type) {
+            case SDL_QUIT:
+                should_exit_ = true;
+                break;
+            case SDL_KEYDOWN: // back-to-back cases like this
+            case SDL_KEYUP:   // give both cases the same behavior
+                set_key_status_from_event(event);
+                break;
         }
     }
 }
 
 bool InputManager::should_exit() {
     return should_exit_;
+}
+
+bool InputManager::track(SDL_Keycode key) {
+    if (key_states_.count(key) > 0) {
+        return false;
+    }
+    else {
+        key_states_.insert(std::pair<SDL_Keycode, KeyStatus>(key, RELEASED));
+        return true;
+    }
+}
+
+KeyStatus InputManager::get_status(SDL_Keycode key) {
+    if (key_states_.count(key) > 0) {
+        return key_states_.at(key);
+    }
+    else {
+        return UNTRACKED;
+    }
+}
+
+void InputManager::set_key_status_from_event(SDL_Event& event) {
+    if (event.type == SDL_KEYDOWN) {
+        auto key = event.key.keysym.sym;
+        if (key_states_.count(key) > 0) {
+            key_states_[key] = JUST_PRESSED;
+        }
+    }
+    if (event.type == SDL_KEYUP) {
+        auto key = event.key.keysym.sym;
+        if (key_states_.count(key) > 0) {
+            key_states_[key] = JUST_RELEASED;
+        }
+    }
 }
--- src/util/input_manager.h
+++ src/util/input_manager.h
@@ -4,9 +4,23 @@
 
 #pragma once
 
+#include <SDL.h>
+#include <unordered_map>
+
+
+enum KeyStatus {
+    JUST_PRESSED,
+    JUST_RELEASED,
+    PRESSED,
+    RELEASED,
+    UNTRACKED
+};
+
 class InputManager {
 private:
+    std::unordered_map<SDL_Keycode, KeyStatus> key_states_;
     bool should_exit_;
+
 public:
     InputManager();
 
@@ -16,5 +30,17 @@ public:
     // returns true if an exit event has been polled
     bool should_exit();
 
-    //TODO: implement input handling methods
+    // returns true if key successfully added to track list, false otherwise
+    bool track(SDL_Keycode key);
+
+    // returns the status of the given key,
+    // JUST_PRESSED/JUST_RELEASED if this is the first frame since press/release
+    // PRESSED/RELEASED if this is not the first frame since press/release
+    // UNTRACKED if the key hasn't been registered with track()
+    KeyStatus get_status(SDL_Keycode key);
+
+private:
+
+    void set_key_status_from_event(SDL_Event& event);
+
 };
