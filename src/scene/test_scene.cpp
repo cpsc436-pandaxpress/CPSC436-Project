5a876d85c36d491b6ba6ee76b7d0a8b5b338f96d
Cowan Cumming <ctcumming@gmail.com>
2019-01-30 23:41:19 -0800

    Panda now collides with platform and stops.

--- src/scene/test_scene.cpp
+++ src/scene/test_scene.cpp
@@ -2,10 +2,14 @@
 // Created by alex on 29/01/19.
 //
 
-#include <components/physics.h>
+#include <components/panda.h>
+#include <components/platform.h>
 #include "test_scene.h"
 
 #include "components/transform.h"
+
+void create_panda();
+void create_platforms();
 
 TestScene::TestScene(Blackboard& blackboard, SceneManager& scene_manager) :
     Scene(scene_manager),
@@ -13,31 +17,13 @@ TestScene::TestScene(Blackboard& blackboard, SceneManager& scene_manager) :
     sprite_transform_system(),
     physics_system()
 {
-    test_entity = registry_.create();
-
-    auto texture = blackboard.textureManager.get_texture("panda");
-    auto shader = blackboard.shader_manager.get_shader("sprite");
-
-    registry_.assign<Transform>(test_entity, 0., 0., 0., 0.5, 0.5);
-    registry_.assign<Sprite>(test_entity, texture, shader);
-    registry_.assign<Physics>(test_entity, true);
-
-    auto platform_texture = blackboard.textureManager.get_texture("platform");
-    float scale = 100.f / platform_texture.width();
-
-    for (int i = 0; i < 5; i++) {
-        auto platform = registry_.create();
-
-        registry_.assign<Transform>(platform, 200 * i - 200., 200., 0., scale, scale);
-        registry_.assign<Sprite>(platform, platform_texture, shader);
-
-        platforms.push_back(platform);
-    }
+    create_panda(blackboard);
+    create_platforms(blackboard);
 }
 
 void TestScene::update(Blackboard& blackboard) {
     // some sample input handling
-    auto& position = registry_.get<Transform>(test_entity);
+    auto& position = registry_.get<Transform>(panda);
 
     if (blackboard.input_manager.key_pressed(SDL_SCANCODE_UP)) {
         position.y -= 1;
@@ -62,4 +48,33 @@ void TestScene::render(Blackboard& blackboard) {
     // update the rendering systems
     sprite_render_system.update(blackboard, registry_);
 
+}
+
+void TestScene::create_panda(Blackboard& blackboard) {
+    panda = registry_.create();
+
+    auto texture = blackboard.textureManager.get_texture("panda");
+    auto shader = blackboard.shader_manager.get_shader("sprite");
+
+    float scale = 0.5;
+    registry_.assign<Transform>(panda, 0., 0., 0., scale, scale);
+    registry_.assign<Sprite>(panda, texture, shader);
+    registry_.assign<Panda>(panda, texture.width() * scale, texture.height() * scale);
+
+}
+
+void TestScene::create_platforms(Blackboard& blackboard) {
+    auto texture = blackboard.textureManager.get_texture("platform");
+    auto shader = blackboard.shader_manager.get_shader("sprite");
+    float scale = 100.f / texture.width();
+
+    for (int i = 0; i < 5; i++) {
+        auto platform = registry_.create();
+
+        registry_.assign<Transform>(platform, 200 * i - 200., 200., 0., scale, scale);
+        registry_.assign<Sprite>(platform, texture, shader);
+        registry_.assign<Platform>(platform, texture.width() * scale, texture.height() * scale);
+
+        platforms.push_back(platform);
+    }
 }
--- src/systems/physics_system.cpp
+++ src/systems/physics_system.cpp
@@ -2,24 +2,51 @@
 // Created by cowan on 30/01/19.
 //
 
+#include "components/platform.h"
 #include "physics_system.h"
-#include "../components/physics.h"
+#include "components/panda.h"
 #include "components/transform.h"
+
+bool checkCollision(Panda pa, Transform pa_tr, Platform pl, Transform pl_tr);
 
 PhysicsSystem::PhysicsSystem() {}
 
 void PhysicsSystem::update(Blackboard &blackboard, entt::DefaultRegistry& registry) {
-    auto view = registry.view<Physics, Transform>();
+    auto view = registry.view<Panda, Transform>();
+
+    auto pl_view = registry.view<Platform, Transform>();
 
     for (auto entity: view) {
-        auto& physics = view.get<Physics>(entity);
+        auto& panda = view.get<Panda>(entity);
         auto& transform = view.get<Transform>(entity);
 
+        for (auto pl_entity: pl_view) {
+            if (panda.grounded)
+                break;
 
-        if (physics.gravity) {
-            physics.y_velocity += GRAVITY;
+            auto& platform = pl_view.get<Platform>(pl_entity);
+            auto& pl_transform = pl_view.get<Transform>(pl_entity);
+
+            if (checkCollision(panda, transform, platform, pl_transform)) {
+                transform.y = pl_transform.y - panda.height;
+                panda.grounded = true;
+                panda.y_velocity = 0;
+            }
         }
 
-        transform.y += physics.y_velocity;
+        if (!panda.grounded) {
+            panda.y_velocity += GRAVITY;
+        }
+
+        transform.x += panda.x_velocity;
+        transform.y += panda.y_velocity;
     }
+}
+
+bool checkCollision(Panda pa, Transform pa_tr, Platform pl, Transform pl_tr) {
+    return
+        pa_tr.x <= pl_tr.x + pl.width &&
+        pa_tr.x + pa.width >= pl_tr.x &&
+        pa_tr.y <= pl_tr.y + pl.height &&
+        pa_tr.y + pa.height >= pl_tr.y;
 }
