7dfd3a5283eb1ddb4d229fadca7b822f58d96276
Alex Hoar <thealexhoar@gmail.com>
2019-02-04 12:50:39 -0800

    Implemented variable jumps. Modified ObeysGravity to hold a factor instead of absolute gravity. Moved absolute gravity to physics_system.h. Modified default panda gravity to double normal.

--- src/systems/player_movement_system.cpp
+++ src/systems/player_movement_system.cpp
@@ -8,40 +8,57 @@
 #include "components/velocity.h"
 #include "components/interactable.h"
 #include "components/transform.h"
+#include "components/obeys_gravity.h"
 
 
 PlayerMovementSystem::PlayerMovementSystem() {}
 
 void PlayerMovementSystem::update(Blackboard &blackboard, entt::DefaultRegistry& registry) {
-    auto view = registry.view<Panda, Transform, Velocity, Interactable>();
+    auto view = registry.view<Panda, Transform, Velocity, Interactable, ObeysGravity>();
     for (auto entity: view) {
         auto &panda = view.get<Panda>(entity);
         auto &transform = view.get<Transform>(entity);
         auto &velocity = view.get<Velocity>(entity);
         auto &walkable = view.get<Interactable>(entity);
+        auto &gravity = view.get<ObeysGravity>(entity);
+
+        if(!walkable.grounded) {
+            if(holding_jump && blackboard.input_manager.key_released(SDL_SCANCODE_SPACE)) {
+                holding_jump = false;
+            }
+            time_since_jump += blackboard.delta_time;
+        }
+
+        if (holding_jump && time_since_jump < 0.6f) {
+            gravity.gravityFactor = 0.3f;
+        }
+        else {
+            gravity.gravityFactor = 1.5f;
+        }
+
+
 
         /*
          * Walking Left and Right
          */
-
-        if (blackboard.input_manager.key_just_pressed(SDL_SCANCODE_LEFT)) {
-            velocity.x_velocity = -PANDA_SPEED;
-        } else if (blackboard.input_manager.key_just_pressed(SDL_SCANCODE_RIGHT)) {
-            velocity.x_velocity = PANDA_SPEED;
-        } else if (blackboard.input_manager.key_just_released(SDL_SCANCODE_LEFT) ||
-                   blackboard.input_manager.key_just_released(SDL_SCANCODE_RIGHT)) {
-            velocity.x_velocity = 0.f;
+        float vx = 0;
+        if (blackboard.input_manager.key_pressed(SDL_SCANCODE_LEFT)) {
+            vx -= PANDA_SPEED;
+        } else if (blackboard.input_manager.key_pressed(SDL_SCANCODE_RIGHT)) {
+            vx += PANDA_SPEED;
         }
+
+        velocity.x_velocity = vx;
 
         /*
          * Jumping
          */
 
         if (walkable.grounded && blackboard.input_manager.key_just_pressed(SDL_SCANCODE_SPACE)) {
-
             walkable.grounded = false;
             velocity.y_velocity = -PANDA_JUMP_SPEED;
-
+            time_since_jump = 0.f;
+            holding_jump = true;
         }
 
 
