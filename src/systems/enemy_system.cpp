ce67f5eb2988f6f6a9c0232a3fc54fa3395f8ec9
Rebecca Roth <rebeccadroth@gmail.com>
2019-03-03 23:56:26 -0800

    remove old enemy entities vertical level

--- src/systems/enemy_system.cpp
+++ src/systems/enemy_system.cpp
@@ -87,6 +87,7 @@ void EnemySystem::update(Blackboard &blackboard, entt::DefaultRegistry& registry
         }
 
     } else if (sceneid == VERTICAL_SCENE_ID) {
+        // Start bread that has entered the screen, delete bread that is no longer playable
         auto bread_view = registry.view<Bread, Transform, Velocity, Collidable>();
         for (auto enemy_entity : bread_view) {
             auto &bread = bread_view.get<Bread>(enemy_entity);
@@ -110,6 +111,20 @@ void EnemySystem::update(Blackboard &blackboard, entt::DefaultRegistry& registry
             }
         }
 
+        // Destroy ghosts that are no longer playable
+        auto ghost_view = registry.view<Ghost, Transform, Collidable>();
+        for (auto enemy_entity : ghost_view) {
+            auto &ghost = ghost_view.get<Ghost>(enemy_entity);
+            auto &ghost_transform = ghost_view.get<Transform>(enemy_entity);
+            auto &ghost_collidable = ghost_view.get<Collidable>(enemy_entity);
+
+            if (ghost_transform.x + ghost_collidable.width < cam_position.x - cam_size.x / 2 ||
+                ghost_transform.y - ghost_collidable.height > cam_position.y + cam_size.y / 2) {
+                registry.destroy(enemy_entity);
+                break;
+            }
+        }
+
         // Create llama projectiles and destroy llamas that are no longer playable
         auto llama_view = registry.view<Llama, Transform, Collidable, Timer>();
         for (auto enemy_entity : llama_view) {
@@ -119,7 +134,8 @@ void EnemySystem::update(Blackboard &blackboard, entt::DefaultRegistry& registry
             auto& llama_timer = llama_view.get<Timer>(enemy_entity);
 
             if (llama_transform.x + llama_collidable.width < cam_position.x - cam_size.x / 2 ||
-                llama_transform.y - llama_collidable.height > cam_position.y + cam_size.y / 2) {
+                llama_transform.y - llama_collidable.height > cam_position.y + cam_size.y / 2 ||
+                llama_transform.x + llama_collidable.width / 2 > cam_position.x + cam_size.x / 2) {
                 registry.destroy(enemy_entity);
                 break;
             }
@@ -130,6 +146,21 @@ void EnemySystem::update(Blackboard &blackboard, entt::DefaultRegistry& registry
             if(llama_timer.is_done(SPIT_TIMER_LABEL)) {
                 generateProjectile(llama_transform.x, llama_transform.y, llama.left, blackboard, registry);
                 llama_timer.reset_watch(SPIT_TIMER_LABEL);
+            }
+        }
+
+        // Destroy spit that are no longer playable
+        auto spit_view = registry.view<Spit, Transform, Collidable>();
+        for (auto enemy_entity : spit_view) {
+            auto &spit = spit_view.get<Spit>(enemy_entity);
+            auto &spit_transform = spit_view.get<Transform>(enemy_entity);
+            auto &spit_collidable = spit_view.get<Collidable>(enemy_entity);
+
+            if (spit_transform.x + spit_collidable.width < cam_position.x - cam_size.x / 2 ||
+                spit_transform.y - spit_collidable.height > cam_position.y + cam_size.y / 2 ||
+                spit_transform.x + spit_collidable.width / 2 > cam_position.x + cam_size.x / 2) {
+                registry.destroy(enemy_entity);
+                break;
             }
         }
     }
