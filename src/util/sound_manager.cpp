0349fb38e4b4b3ec5a21ac2d8108b3c28fc9c702
x4r1b <kennethwmccorkell@gmail.com>
2019-03-06 13:47:59 -0800

    Sounmanager class created

---
+++ src/util/sound_manager.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Kenneth William on 2019-03-06.
+//
+
+#include "sound_manager.h"
+
+SoundManager::SoundManager() {}
+
---
+++ src/util/sound_manager.h
@@ -0,0 +1,18 @@
+//
+// Created by Kenneth William on 2019-03-06.
+//
+
+#ifndef PANDAEXPRESS_SOUND_MANAGER_H
+#define PANDAEXPRESS_SOUND_MANAGER_H
+
+
+class SoundManager {
+
+public:
+    SoundManager();
+
+
+};
+
+
+#endif //PANDAEXPRESS_SOUND_MANAGER_H
