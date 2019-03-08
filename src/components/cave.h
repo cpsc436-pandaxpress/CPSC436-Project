206c824bfe0780c9eaeac468c0efaf3f83303854
Rebecca Roth <rebeccadroth@gmail.com>
2019-03-07 21:49:43 -0800

    add mesh file for cave

---
+++ src/components/cave.h
@@ -0,0 +1,16 @@
+//
+// Created by Becca Roth on 2019-03-02.
+//
+
+#ifndef PANDAEXPRESS_CAVE_H
+#define PANDAEXPRESS_CAVE_H
+
+#include "util/constants.h"
+
+struct Cave {
+    SceneID sceneID;
+
+    Cave() : sceneID(1) {}
+};
+
+#endif //PANDAEXPRESS_CAVE_H
