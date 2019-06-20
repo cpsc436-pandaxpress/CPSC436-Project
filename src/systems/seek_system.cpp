09a9a8f3b52c7ddefed377c3b5d4eef7aed93303
x4r1b <kennethwmccorkell@gmail.com>
2019-06-19 20:24:02 -0700

    Fixed bat seeking bug

--- src/systems/seek_system.cpp
+++ src/systems/seek_system.cpp
@@ -75,22 +75,22 @@ void SeekSystem::update(Blackboard &blackboard, entt::DefaultRegistry& registry)
                 if(seeks.batDirection==Seeks::LEFT){
                     velocity.x_velocity = -seeks.seek_speed;
                     velocity.y_velocity = 0;
-                    transform.y = transform.y + sin(transform.x/50)*0.3f;
+                    transform.y = transform.y + sin(transform.x/50)*0.6f;
                     transform.x_scale = abs(transform.x_scale);
                 }else if(seeks.batDirection==Seeks::RIGHT){
                     velocity.x_velocity = seeks.seek_speed;
                     velocity.y_velocity = 0;
-                    transform.y = transform.y + sin(transform.x/50)*0.3f;
+                    transform.y = transform.y + sin(transform.x/50)*0.6f;
                     transform.x_scale = -abs(transform.x_scale);
                 }else if(seeks.batDirection==Seeks::UP) {
                     velocity.y_velocity = -seeks.seek_speed;
                     velocity.x_velocity = 0;
-                    transform.x= transform.x + sin(transform.y/50)*0.3f;
+                    transform.x= transform.x + sin(transform.y/50)*0.6f;
                     transform.set_angle_degrees(90);
                 }else if(seeks.batDirection==Seeks::DOWN) {
                     velocity.y_velocity = seeks.seek_speed;
                     velocity.x_velocity = 0;
-                    transform.x= transform.x + sin(transform.y/50)*0.3f;
+                    transform.x= transform.x + sin(transform.y/50)*0.6f;
                     transform.set_angle_degrees(-90);
                 }
 
